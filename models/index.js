ce72410bc1b6f559653ea184b038e1aa28c0b512
Nick Brennan <nick_p_b@yahoo.com>
2016-04-02 12:40:10 -0700

    rename

--- models/index.js
+++ models/index.js
@@ -3,4 +3,4 @@ mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
                   "mongodb://localhost/personal-api");
 
-module.exports.Guitar = require("./guitar.js");
+module.exports.Guitar = require("./electric_guitar.js");
