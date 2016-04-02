e5644bc7cd01656eb9f552325f1474cedb905a8d
Nick Brennan <nick_p_b@yahoo.com>
2016-04-02 12:03:40 -0700

    another typo

--- models/index.js
+++ models/index.js
@@ -3,4 +3,4 @@ mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
                   "mongodb://localhost/personal-api");
 
-module.exports.Guitar = require("./guitar");
+module.exports.Guitar = require("./guitar.js");
