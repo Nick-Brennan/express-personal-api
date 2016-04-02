933b07f12cf244bd3b2cd6df231dd0a9d53c5150
Nick Brennan <nick_p_b@yahoo.com>
2016-04-02 12:21:25 -0700

    fixing seed data

--- seed.js
+++ seed.js
@@ -5,13 +5,18 @@ var new_guitar = {
                   model: "Telecaster",
                   pickups: "lil' 59 - Bridge, Stock - Neck",
                   neck_construction: "Bolt-on"
-                }
+                };
+  db.Guitar.remove({}, function(err, books){
+    if(err) {
+      console.log('Error occurred in remove', err);
+    } else {
+      console.log('removed all books');
 
-db.Guitar.create(new_guitar, function(err, guitar){
-  if (err){
-    return console.log("Error:", err);
-  }
-
-  console.log("Created new guitar", guitar_id)
-  process.exit(); // we're all done! Exit the program.
-})
+      // create new records based on the array books_list
+      db.Guitar.create(new_guitar, function(err, guitar){
+        if (err) { return console.log('err', err); }
+        console.log("created a new guitar with id: ", guitar.id);
+        process.exit();
+      });
+    }
+  });
