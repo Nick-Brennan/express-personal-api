74f24a054b96f38b368f1fed9d78f002d590b04e
Nick Brennan <nick_p_b@yahoo.com>
2016-04-03 16:37:22 -0700

    building dashboard at root

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,52 @@
 console.log("Sanity Check: JS is working!");
-
+var profileTemplate;
+var guitarTemplate;
+var guitarsTarget;
+var nickTarget;
+var profile;
+var guitars = [];
 $(document).ready(function(){
+  nickTarget = $('#profileTarget');
+  guitarsTarget = $('#guitarsTarget');
+  var nickSource = $('#nick-template').html();
+  var guitarSource = $('#guitar-template').html();
+  profileTemplate = Handlebars.compile(nickSource);
+  guitarTemplate = Handlebars.compile(guitarSource);
 
-// your code
-
+  $.ajax({
+    method: 'GET',
+    url: '/profile',
+    success: handleProfileData
+  });
+  $.ajax({
+    method: 'GET',
+    url: '/guitars',
+    success: handleGuitarData
+  });
 });
+
+function handleProfileData(data){
+  profile = data;
+  console.log(profile);
+  renderProfile();
+}
+
+function renderProfile(){
+  console.log('rendering');
+  nickTarget.empty();
+  var profileHtml = profileTemplate({ profile: profile });
+  console.log(profileHtml)
+  nickTarget.append(profileHtml);
+};
+
+function handleGuitarData(data){
+  guitars = data;
+  console.log(guitars);
+  renderGuitars();
+}
+
+function renderGuitars(){
+  guitarsTarget.empty();
+  var guitarHtml = guitarTemplate({guitars: guitars});
+  guitarsTarget.append(guitarHtml);
+}
